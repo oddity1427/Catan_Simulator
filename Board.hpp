ca8965848e02508a165e73bf9019c7e6703f051a
Nick <wolfornd@miamioh.edu>
2018-01-29 15:18:42 -0500

    updated board to link nodes to tiles as well, updated main to be able to check if all the files compile

--- Board.hpp
+++ Board.hpp
@@ -4,15 +4,23 @@
 //board is going to be used to avoid circular inclusions between roads and nodes, and allow for random access by players instead of searching through as a linked list
 #include "Road.hpp"
 #include "Node.hpp"
+#include "Tile.hpp"
 #include <map>
+#include <vector>
 
 struct Board
 {
 	
 	//if you have a road you need to be able to find the roads that are connected to it and vice versa. maps used here to find the vector of connections;
-	std::map<Road&, std::vector<Node&>> road_Connect;
+	//nodes also need to connect to Tiles
+	//TODO: find out why compiler error when I make the vectors of references to an instance instead of just to an instance
+	std::map<Road, std::vector<Node > > road2node;
 
-	std::map<Node&, std::vector<Road&>> node_Connect;
+	std::map<Node, std::vector<Road > > node2road;
+
+	std::map<Node, std::vector<Tile > > node2tile;
+
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-//just a placeholder for a while, probably going to be used for testing later
-//for now there isn't much to test while the other classes are being written;
+//Right now just being used to check if all of the files will compile or if I've done something really wrong
+
+#include "Board.hpp"
+#include "Game.hpp"
+#include "Node.hpp"
+#include "Player.hpp"
+#include "Resource.hpp"
+#include "Road.hpp"
+#include "Tile.hpp"
+
 int main(){
-	return;
+	return 1;
 }
