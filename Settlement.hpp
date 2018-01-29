65bd491e5735f0a7c50c067c80be5daedae14ceb
Nick <wolfornd@miamioh.edu>
2018-01-29 00:29:14 -0500

    wrote class for Settlement

--- Settlement.hpp
+++ Settlement.hpp
@@ -1,4 +1,18 @@
 #ifndef _SETTLEMENT_HPP
 #define _SETTLEMENT_HPP
 
+#include "Player.hpp"
+
+//there are only 2 levels that a settlement can have, it will be useful to be able to reference them directly
+int const SETTLEMENT 	= 0;
+int const CITY 			= 1;
+
+//Not sure how I am going to implement player actions yet, Settlement really might only end up keeping track of this information
+struct Settlement{
+
+	int level;
+	Player owner;
+
+};
+
 #endif
