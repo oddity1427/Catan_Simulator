a7acc0e3278a6fed9de9341da70800eeba27c700
Nick <wolfornd@miamioh.edu>
2018-01-29 00:22:57 -0500

    fleshed out tile and player as classes, their headers are written and commented

--- Player.hpp
+++ Player.hpp
@@ -1,4 +1,20 @@
 #ifndef _PLAYER_HPP
 #define _PLAYER_HPP
 
+//There are only ever four players.
+int const PLAYER_RED 	= 0;
+int const PLAYER_BLUE 	= 1;
+int const PLAYER_WHITE 	= 2;
+int const PLAYER_ORANGE = 3;
+
+//right now there is not much to players other than that they exist
+//this will just be used currently so that buildings and roads can have owners
+//players will be able to do more things in the future, but this is not necessary right now
+//I may actually later make this a parent of derived human/cpu players, but that will come later
+struct Player{
+
+	int color;
+
+};
+
 #endif
--- Tile.hpp
+++ Tile.hpp
@@ -1,4 +1,18 @@
 #ifndef _TILE_HPP
 #define _TILE_HPP
 
+//define names for all of the tile types in the game
+int const DESERT_TILE 	= 0;
+int const SHEEP_TILE 	= 1;
+int const BRICK_TILE 	= 2;
+int const WOOD_TILE 	= 3;
+int const WHEAT_TILE 	= 4;
+int const STONE_TILE 	= 5;
+
+//each tile just has this information that other classes will access
+struct Tile {
+	int resourceType;
+	int dieNumber;
+};
+
 #endif
