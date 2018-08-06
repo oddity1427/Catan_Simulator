722612ceffd5b69d9aac513a4ceecb13cb08a0fc
Nick Wolford <wolfornd@miamioh.edu>
2018-08-05 23:51:33 -0400

    Road* updated; Road, Tile, Node all finished for now; Board can now be written;

--- Road.cpp
+++ Road.cpp
@@ -2,11 +2,39 @@
 #define ROAD_CPP_FLAG
 
 #include "Road.hpp"
+#include <iostream>
 
 Road::Road(int ID){
 	id = ID;
-	state = NO_ROAD;
+	taken = false;
+}
 
+//claiming does not check if the road is taken because the player interface will have to check anyway
+void Road::setOwner(Player * p){
+	owner = p; 
+}
+
+void Road::addNode(Node * nodeP){
+	conNodes.push_back(nodeP);
+}
+
+bool Road::isTaken(){
+	return taken;
+}
+
+Player * Road::getOwner(){
+	return owner;
+}
+
+Node * Road::getOtherNode(Node * n_p){
+	for(unsigned i = 1; i < 3; i++){
+		if(conNodes[i] != n_p){
+			return(conNodes[i]);
+		}
+	}
+	//not really a great way to do errors, but simple, and works for something that I see in advance has the potential to crash
+	//this comment will stand as a marker for placs that have priority when implementing error handling 
+	std::cout << "Error, all Nodes * in conNodes are equal to input Node *  ; Road.cpp:35";
 }
 
 #endif
--- Road.hpp
+++ Road.hpp
@@ -1,20 +1,31 @@
 #ifndef ROAD_HPP_FLAG
 #define ROAD_HPP_FLAG
 
-#include "Player.hpp"
+//#include "Player.hpp"
+#include <vector>
 
-int const NO_ROAD 	= 0;
-int const ROAD 		= 1;
+class Node;
+class Player; 
 
-//This seems a little bit rediculous but having a type will make this more obvious and readable in other classes;	
 class Road{
-	public:
-
+private:
 	int id;
-	int state;
-	Player* owner; 
+	bool taken;
+	Player* owner;
+	std::vector<Node *> conNodes;
 
+public:
 	Road(int);
+
+	void setOwner(Player *);
+	void addNode(Node *);
+
+	Node * getOtherNode(Node *);
+	Player * getOwner();
+	bool isTaken();
+
+
+
 };
 
 #endif
